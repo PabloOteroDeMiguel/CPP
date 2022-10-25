ecc893a7c607ad26b759b7318cdf376743acfa4a
Pablo Otero <potero91@gmail.com>
2022-10-25 11:50:19 +0200

    ex00

--- CPP_01/ex00/Zombie.cpp
+++ CPP_01/ex00/Zombie.cpp
@@ -6,8 +6,30 @@
 /*   By: potero-d <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/25 10:18:41 by potero-d          #+#    #+#             */
-/*   Updated: 2022/10/25 10:18:45 by potero-d         ###   ########.fr       */
+/*   Updated: 2022/10/25 11:49:53 by potero-d         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "Zombie.hpp"
 
+Zombie::Zombie(std::string name) : _name(name) { 
+	
+	announce();
+	return ;
+}
+
+Zombie::~Zombie(void) {
+	
+	std::cout << Zombie::getName() << " has been destroyed" << std::endl;
+	return ;
+}
+
+void	Zombie::announce(void) {
+
+	std::cout << Zombie::getName() << " BraiiiiiiinnnzzzZ..." << std::endl;
+}
+
+std::string	Zombie::getName(void) {
+
+	return (this->_name);
+}
--- CPP_01/ex00/Zombie.hpp
+++ CPP_01/ex00/Zombie.hpp
@@ -6,7 +6,7 @@
 /*   By: potero-d <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/25 10:18:49 by potero-d          #+#    #+#             */
-/*   Updated: 2022/10/25 10:41:23 by potero-d         ###   ########.fr       */
+/*   Updated: 2022/10/25 11:49:55 by potero-d         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,5 +17,20 @@
 #include <iostream>
 #include <cctype>
 #include <stdlib.h>
+
+class Zombie {
+	public:
+		Zombie(std::string name);
+		~Zombie(void);
+		void	announce(void);
+		std::string	getName(void);
+	//	Zombie* newZombie(std::string name);
+
+	private:
+		std::string	_name;	
+};
+
+
+//Mirar new c++
 
 #endif
