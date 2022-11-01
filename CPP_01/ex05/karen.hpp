cc6518f8c8d2e8196dbd4d9a50dda79a37da16de
Pablo Otero <potero91@gmail.com>
2022-11-01 11:45:11 +0100

    ex05

--- CPP_01/ex05/karen.hpp
+++ CPP_01/ex05/karen.hpp
@@ -6,7 +6,7 @@
 /*   By: potero-d <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/31 11:49:58 by potero-d          #+#    #+#             */
-/*   Updated: 2022/11/01 10:29:49 by potero-d         ###   ########.fr       */
+/*   Updated: 2022/11/01 11:44:43 by potero-d         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,13 +15,14 @@
 
 #include<iostream>
 #include<ostream>
+#include<string.h>
 
 
 class	Karen {
 	public:
 		Karen(void);
 		~Karen(void);
-		void		complain( std::string level );
+		void		complain(std::string level);
 		void		setName(std::string);
 		std::string	getName(void);
 		void		test(std::string str);
@@ -30,13 +31,13 @@ class	Karen {
 	private:
 		std::string	_name;
 
-		void		debug( void );
-		void		info( void );
-		void		warning( void );
-		void		error( void );
+		void		debug(void);
+		void		info(void);
+		void		warning(void);
+		void		error(void);
+		void		what(void);
 };
 
-typedef	void (Karen::* pntr2)(std::string);
-//typedef	void (Karen::* pntr)(std::string);
+typedef	void (Karen::* pointer)();
 
 #endif
--- CPP_01/ex05/main.cpp
+++ CPP_01/ex05/main.cpp
@@ -6,7 +6,7 @@
 /*   By: potero-d <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/31 11:49:34 by potero-d          #+#    #+#             */
-/*   Updated: 2022/11/01 10:29:53 by potero-d         ###   ########.fr       */
+/*   Updated: 2022/11/01 11:44:44 by potero-d         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,21 +15,11 @@
 int	main() {
 
 	Karen	karen;
-	//tengo que creear un typedef para hacer un array de funciones puntero y otra array con las palabras clave
-	pntr2 pnt2 = &Karen::test2;
-//	pntr pnt1 = &Karen::test;
+	std::string	level;
 
-//	(karen.*pnt1) ("shit");
-	(karen.*pnt2) ("good shit");
-
-	pntr2 pnt1 = &Karen::test;
-	(karen.*pnt1) ("good shit");
-
-	pntr2	arry[2];
-
-	arry[0] = &Karen::test2;
-	arry[1] = &Karen::test;
-	(karen.*arry[0]) ("good shit");
-	(karen.*arry[1]) ("good shit");
+	std::cout << "What is Karen yelling? " << std::endl;
+	std::cin >> level;
+	karen.complain(level);
 }
+
 	
