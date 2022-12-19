cdf59a1d527b8515183da1402de33267c65245ce
Pablo Otero <potero91@gmail.com>
2022-12-19 16:32:23 +0100

    operator <<

--- CPP_05/ex01/Form.cpp
+++ CPP_05/ex01/Form.cpp
@@ -5,39 +5,80 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: potero-d <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/12/19 15:17:14 by potero-d          #+#    #+#             */
-/*   Updated: 2022/12/19 15:32:35 by potero-d         ###   ########.fr       */
+/*   Created: 2022/12/19 16:08:44 by potero-d          #+#    #+#             */
+/*   Updated: 2022/12/19 16:31:08 by potero-d         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef FORM_HPP
-# define FORM_HPP
+#include "Form.hpp"
 
-#include "Bureaucrat.hpp"
+Form::Form(void) :  _requiredSign(150), _requiredExecute(150) {
 
-class Form {
+	this->_signed = false;
+	return;
+}
 
-	public:
+Form::Form(std::string name, int requiredSign, int requiredExecute) :
+			_name(name), _requiredSign(requiredSign), _requiredExecute(requiredExecute) {
 
-		//Form();
-		Form(std::string name, int requiredSign, _requiredExecute);
-		~Form();
-		Form(Formm& cpy);
-		Form& operator=(Form& rhs);
+//	this->_name = name;
+	this->_signed = false;
+/*
+	if (requiredSign < 1)
+		throw From::GradeTooHighException();
+	else if (requiredSign > 150)
+		throw From::GradeTooLowException();
+	else*/
+//		this->_requiredSign = requiredSign;
+/*	if (requiredExecute < 1)
+		throw From::GradeTooHighException();
+	else if (requiredExecute > 150)
+		throw From::GradeTooLowException();
+	else*/
+//		this->_requiredExecute = requiredExecute;
 
-		std::string	getName() const;
-		bool		getSigned();
-		int			getRequiredSign() const;
-		int			getRequiredExecute() const;
+	std::cout << *this << std::endl;
+	return;
+}
 
-	private:
+Form::~Form(void) {
 
-		std::string const	_name;
-		bool				_signed;
-		int const			_requiredSign;
-		int const			_requiredExecute;
-};
+	std::cout << "Form " << this->_name << " destroyed." << std::endl;
+}
 
-std::ostream& operator<<(std::ostream& out, const Form& form);
 
-#endif
+std::string	Form::getName() const {
+
+	return(this->_name);
+}
+
+bool	Form::getSigned() const {
+
+	return(this->_signed);
+}
+
+int	Form::getRequiredSign() const {
+
+	return(this->_requiredSign);
+}
+
+int	Form::getRequiredExecute() const {
+
+	return(this->_requiredExecute);
+}
+
+std::ostream& operator<<(std::ostream& out, const Form& form) {
+
+	std::string	sig;
+
+	if (form.getSigned() == true)
+		sig = "\033[1;32mSIGNED.\033[0m";
+	else
+		sig = "\033[0;31mNOT SIGNED.\033[0m";
+
+	out << "Form: " << form.getName() << "." << '\n'
+		<< '\t' << "Grade required to sign: " << form.getRequiredSign() << '\n'
+		<< '\t' << "Grade required to execute: " << form.getRequiredExecute() << '\n' 
+		<< sig;
+	return(out);
+}
--- CPP_05/ex01/Form.hpp
+++ CPP_05/ex01/Form.hpp
@@ -5,16 +5,39 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: potero-d <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/12/19 15:17:10 by potero-d          #+#    #+#             */
-/*   Updated: 2022/12/19 15:37:41 by potero-d         ###   ########.fr       */
+/*   Created: 2022/12/19 16:08:32 by potero-d          #+#    #+#             */
+/*   Updated: 2022/12/19 16:15:26 by potero-d         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "Form.hpp"
+#ifndef FORM_HPP
+# define FORM_HPP
 
-Form::Form(std::string name, int requiredSign, _requiredExecute) {
+#include "Bureaucrat.hpp"
 
-	this->_name = name;
-	this->_signed = false;
-	this->
+class Form {
 
+	public:
+
+		Form();
+		Form(std::string name, int requiredSign, int requiredExecute);
+		~Form();
+		//Form(Form& cpy);
+		//Form& operator=(Form& rhs);
+
+		std::string	getName() const;
+		bool		getSigned() const;
+		int			getRequiredSign() const;
+		int			getRequiredExecute() const;
+
+	private:
+
+		std::string const	_name;
+		bool				_signed;
+		int const			_requiredSign;
+		int const			_requiredExecute;
+};
+
+std::ostream& operator<<(std::ostream& out, const Form& form);
+
+#endif
