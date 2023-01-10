e02d74abf1cd15f5fa2a9d520f54a72fcfd2d795
Pablo Otero <potero91@gmail.com>
2023-01-10 13:16:44 +0100

    main

--- CPP_06/ex01/main.cpp
+++ CPP_06/ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: potero-d <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/10 12:12:29 by potero-d          #+#    #+#             */
-/*   Updated: 2023/01/10 13:05:09 by potero-d         ###   ########.fr       */
+/*   Updated: 2023/01/10 13:13:49 by potero-d         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,12 +19,16 @@ int	main(void) {
 	a = serialize(&first);
 
 	Data		*last = deserialize(a);
-//	std::cout << last << std::endl;
+	std::cout << &last <<  " --> ";
 	std::cout << last->getId() << std::endl;
+	std::cout << &first << " --> ";
+	std::cout << first.getId() << std::endl;
 
 	Data		*sum = deserialize(a + 1);
+	Data		*sum2 = deserialize(a + 2);
 	Data		*rest = deserialize(a - 1);
 	sum->getId();
+	sum2->getId();
 	rest->getId();
 
 	return(0);
