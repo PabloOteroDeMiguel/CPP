d5ce68d107f90ac222911913eebdb83220bf480a
Pablo Otero <potero91@gmail.com>
2023-01-11 08:41:31 +0100

    random and try-catch

--- CPP_06/ex02/generate.cpp
+++ CPP_06/ex02/generate.cpp
@@ -6,7 +6,7 @@
 /*   By: potero <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/10 18:46:34 by potero            #+#    #+#             */
-/*   Updated: 2023/01/10 18:51:46 by potero           ###   ########.fr       */
+/*   Updated: 2023/01/11 08:37:04 by potero-d         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,8 +17,11 @@
 
 Base	*generate(void)
 {
-	srand(time(NULL));
-	switch (rand() % 3)
+	int	random;
+
+	srand((unsigned) time(NULL));
+	random = rand() % 3;
+	switch (random)
 	{
 	case 0:
 		return (new A);
@@ -33,11 +36,38 @@ Base	*generate(void)
 
 void	identify(Base *p)
 {
-//	try catch
-	if (dynamic_cast<A*>(p))
+	try
+	{
+		if (dynamic_cast<A*>(p))
+			std::cout << "Is A" << std::endl;
+		else if (dynamic_cast<B*>(p))
+			std::cout << "Is B" << std::endl;
+		else if (dynamic_cast<C*>(p))
+			std::cout << "Is C" << std::endl;
+	}
+	catch(int e)
+	{
+		std::cout << "Is Null" << std::endl;
+	}
+/*
+	try 
+	{
+		dynamic_cast<A*>(p);
 		std::cout << "Is A" << std::endl;
-	else if (dynamic_cast<B*>(p))
+	}
+	try
+	{
+		dynamic_cast<B*>(p);
 		std::cout << "Is B" << std::endl;
-	else if (dynamic_cast<C*>(p))
+	}
+	try
+	{
+		dynamic_cast<C*>(p);
 		std::cout << "Is C" << std::endl;
+	}
+	catch
+	{
+		std::cout << "Is Null" << std::endl;
+	}
+*/
 }
--- CPP_06/ex02/main.cpp
+++ CPP_06/ex02/main.cpp
@@ -6,10 +6,11 @@
 /*   By: potero <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/10 18:48:43 by potero            #+#    #+#             */
-/*   Updated: 2023/01/10 18:51:36 by potero           ###   ########.fr       */
+/*   Updated: 2023/01/11 08:39:33 by potero-d         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <unistd.h>
 #include "Base.hpp"
 #include "A.hpp"
 #include "B.hpp"
@@ -20,15 +21,22 @@ int main(void) {
 	Base *first = generate();
 	std::cout << "First: ";
 	identify(first);
+	sleep(1);
 
 	Base *second = generate();
 	std::cout << "Second: ";
 	identify(second);
+	sleep(1);
 
 	Base *last = generate();
 	std::cout << "Last: ";
 	identify(last);
-
+	sleep(1);
+/*
+	Base *error = generate();
+	std::cout << "Error: ";
+	identify(*error);
+*/
 	delete first;
 	delete second;
 	delete last;
