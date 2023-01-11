5152777458a382f8853e1acca2330b4ed3294d56
Pablo Otero <potero91@gmail.com>
2023-01-11 15:12:24 +0100

    ex00

--- CPP_07/ex00/main.cpp
+++ CPP_07/ex00/main.cpp
@@ -6,13 +6,37 @@
 /*   By: potero-d <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/11 09:00:53 by potero-d          #+#    #+#             */
-/*   Updated: 2023/01/11 09:31:36 by potero-d         ###   ########.fr       */
+/*   Updated: 2023/01/11 15:11:24 by potero-d         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include<iostream>
 #include<ostream>
 #include "whatever.hpp"
+
+
+class Awesome
+{
+	public:
+		Awesome(void) : _n(0) {}
+		Awesome( int n ) : _n( n ) {}
+		Awesome & operator= (Awesome & a) { _n = a._n; return *this; }
+		bool operator==( Awesome const & rhs ) const { return (this->_n == rhs._n); }
+		bool operator!=( Awesome const & rhs ) const{ return (this->_n != rhs._n); }
+		bool operator>( Awesome const & rhs ) const { return (this->_n > rhs._n); }
+		bool operator<( Awesome const & rhs ) const { return (this->_n < rhs._n); }
+		bool operator>=( Awesome const & rhs ) const { return (this->_n >= rhs._n); }
+		bool operator<=( Awesome const & rhs ) const { return (this->_n <= rhs._n); }
+		int get_n() const { return _n; }
+
+	private:
+		int _n;
+};
+
+std::ostream & operator<<(std::ostream & o, const Awesome &a) {
+	o << a.get_n();
+	return o;
+}
 
 int main( void ) {
 
@@ -33,6 +57,16 @@ int main( void ) {
 	std::cout << "max( c, d ) = " << ::max( c, d ) << std::endl;
 
 //	std::cout << "min( a, d ) = " << ::min( a, d ) << std::endl;
+	Awesome e(2), f(42);
+	std::cout << "e: " << e << std::endl;
+	std::cout << "f: " << f << std::endl;
+	swap(e, f);
+	std::cout << "e: " << e << std::endl;
+	std::cout << "f: " << f << std::endl;
+
+	std::cout << max(e, f) << std::endl;
+	std::cout << min(e, f) << std::endl;
+
 
 	return 0;
 }
